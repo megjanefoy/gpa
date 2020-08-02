418bd39dd39e98bd4576c1c3e622fa1238d77985
Megan Jane Thompson <59883929+megjthom@users.noreply.github.com>
2020-08-02 16:25:22 -0500

    Update gpa.c

--- gpa.c
+++ gpa.c
@@ -5,9 +5,6 @@
  * Semester: COP4338 - Spring 2021
  * Author Megan Jane Thompson
  *
- * I affirm that this program is entirely my own work
- * and none of it is the work of any other person.
- *
  * This program calculates the gpa of 6 student courses.
  */
 
